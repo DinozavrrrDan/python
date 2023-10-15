3bf3fae9c58ba0258b95103bcb8b176c3b7ed90c
Dinozavrrr <113333332+DinozavrrrDan@users.noreply.github.com>
2023-10-15 10:33:41 +0300

    Update lab1_3.py

--- Lab1/lab1_3.py
+++ Lab1/lab1_3.py
@@ -1,4 +1,10 @@
 # TODO Найдите количество книг, которое можно разместить на дискете
-disk_memory = 1.44 * 1024 * 1024
-book_size_in_memory = 4 * 25 * 50 * 100
+kilobytes_in_megabyte = 1024
+bytes_in_kilobyte = 1024
+disk_memory = 1.44 * kylpbytes_in_megabyte * bytes_in_kilobyte
+book_pages = 100
+string_on_page = 50 
+symbols_in_string = 25
+memory_one_symbol = 4
+book_size_in_memory = memory_one_symbol * symbols_in_string * string_on_page * book_pages
 print("Количество книг, помещающихся на дискету:", round(disk_memory / book_size_in_memory))
